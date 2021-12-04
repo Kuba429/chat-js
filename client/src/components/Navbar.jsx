99389aa2f535c446f94a64543c6525f5e333dcd1
Kuba429 <jakubzawadzki429@gmail.com>
2021-12-04 15:46:55 +0100

    online user counter works properly

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,28 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MyContext from "../ContextWrapper";
-
 export default function Navbar() {
+    const [onlineCount, setOnlineCount] = useState(0);
     const context = useContext(MyContext);
     const changeUsernameHandler = () => {
         context.setLoginModalState(true);
     };
+    const checkOnlineHandler = () => {
+        context.socket.emit("checkOnline", context.roomId);
+    };
+    context.socket.on("onlineStatus", (data) => {
+        setOnlineCount(data);
+    });
+
+    useEffect(() => {
+        checkOnlineHandler();
+    }, []);
     return (
         <nav id="navbar">
             <span className="username" onClick={changeUsernameHandler}>
                 {context.usernameState}
+            </span>
+            <span className="onlineUsers" onClick={checkOnlineHandler}>
+                In room: <span>{onlineCount || "0"}</span>
             </span>
         </nav>
     );
--- server/index.js
+++ server/index.js
@@ -10,16 +10,30 @@ const server = app.listen(PORT, () => {
 const io = new Server(server);
 
 io.on("connection", (socket) => {
+    const getOnlineInRoom = (roomId) => {
+        return io.sockets.adapter.rooms?.get(roomId)?.size;
+    };
     console.log("user connected");
 
     socket.on("disconnect", () => {
+        io.to(socket.customRoom).emit(
+            "onlineStatus",
+            getOnlineInRoom(socket.customRoom)
+        );
         socket.removeAllListeners();
     });
 
     socket.on("join", (data) => {
         socket.join(data);
+        socket.customRoom = data;
+        io.to(data).emit("onlineStatus", getOnlineInRoom(data));
     });
+
     socket.on("sendMessage", (data) => {
         io.to(data.roomId).emit("receiveMessage", data.message);
+    });
+
+    socket.on("checkOnline", (roomId) => {
+        socket.emit("onlineStatus", getOnlineInRoom(roomId));
     });
 });
