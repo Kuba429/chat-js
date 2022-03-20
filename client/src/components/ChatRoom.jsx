b8620a00c1a41f6d625f6df6945ac14c69c63c62
Kuba429 <jakubzawadzki429@gmail.com>
2022-03-20 20:20:08 +0100

    extracted form to separate component

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -4,49 +4,19 @@ import MyContext from "../ContextWrapper";
 import Navbar from "./Navbar";
 import Message from "./Message";
 import { v4 } from "uuid";
-import GifPanelWrapper from "./GifPanel";
+import InputPanel from "./InputPanel";
 
 export default function ChatRoom() {
     const { roomId } = useParams();
     const context = useContext(MyContext);
     context.roomId = roomId;
-    const inputRef = useRef(null);
     const [messagesState, setMessagesState] = useState([]);
-    const submitHandler = (e) => {
-        e.preventDefault();
-        if (inputRef.current.value.length < 1) return;
-        let message = {
-            author: context.usernameState,
-            authorId: context.socket.id,
-            content: inputRef.current.value,
-            time: new Date().getTime(),
-        };
-        inputRef.current.value = "";
-        resizeHandler();
-        context.socket.emit(`sendMessage`, { roomId, message });
-    };
 
     context.socket.emit("join", roomId);
     context.socket.off("receiveMessage");
     context.socket.once("receiveMessage", (message) => {
         setMessagesState([message, ...messagesState]);
     });
-
-    const resizeHandler = () => {
-        console.log(inputRef.current.scrollHeight);
-        inputRef.current.style.height = "auto";
-        if (inputRef.current.scrollHeight <= 150) {
-            inputRef.current.style.height =
-                inputRef.current.scrollHeight + "px";
-        } else {
-            inputRef.current.style.height = "150px";
-        }
-    };
-    const keyDownHandler = (e) => {
-        if (e.keyCode === 13 && !e.shiftKey) {
-            submitHandler(e);
-        }
-    };
 
     useEffect(() => {
         if (
@@ -68,28 +38,7 @@ export default function ChatRoom() {
                         })}
                     </div>
                 </div>
-                <form className="inputPanel" onSubmit={submitHandler}>
-                    <textarea
-                        className="customScrollbar"
-                        type="text"
-                        placeholder="Type here..."
-                        ref={inputRef}
-                        rows="1"
-                        onInput={resizeHandler}
-                        onKeyDown={keyDownHandler}
-                        onScroll={() => {
-                            console.log(inputRef.current.scrollHeight);
-                        }}
-                    />
-
-                    <button type="button">
-                        <ion-icon name="accessibility-outline"></ion-icon>
-                    </button>
-                    <button type="submit">
-                        <ion-icon name="arrow-forward-outline"></ion-icon>
-                    </button>
-                    <GifPanelWrapper inputRef={inputRef} />
-                </form>
+                <InputPanel />
             </div>
         </>
     );
---
+++ client/src/components/InputPanel.jsx
@@ -0,0 +1,60 @@
+import { useRef } from "react";
+import GifPanelWrapper from "./GifPanel";
+
+const InputPanel = () => {
+    const inputRef = useRef(null);
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (inputRef.current.value.length < 1) return;
+        let message = {
+            author: context.usernameState,
+            authorId: context.socket.id,
+            content: inputRef.current.value,
+            time: new Date().getTime(),
+        };
+        inputRef.current.value = "";
+        resizeHandler();
+        context.socket.emit(`sendMessage`, { roomId, message });
+    };
+    const resizeHandler = () => {
+        console.log(inputRef.current.scrollHeight);
+        inputRef.current.style.height = "auto";
+        if (inputRef.current.scrollHeight <= 150) {
+            inputRef.current.style.height =
+                inputRef.current.scrollHeight + "px";
+        } else {
+            inputRef.current.style.height = "150px";
+        }
+    };
+    const keyDownHandler = (e) => {
+        if (e.keyCode === 13 && !e.shiftKey) {
+            submitHandler(e);
+        }
+    };
+    return (
+        <form className="inputPanel" onSubmit={submitHandler}>
+            <textarea
+                className="customScrollbar"
+                type="text"
+                placeholder="Type here..."
+                ref={inputRef}
+                rows="1"
+                onInput={resizeHandler}
+                onKeyDown={keyDownHandler}
+                onScroll={() => {
+                    console.log(inputRef.current.scrollHeight);
+                }}
+            />
+
+            <button type="button">
+                <ion-icon name="accessibility-outline"></ion-icon>
+            </button>
+            <button type="submit">
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+            </button>
+            <GifPanelWrapper inputRef={inputRef} />
+        </form>
+    );
+};
+
+export default InputPanel;
