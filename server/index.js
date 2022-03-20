e25361bd9efe1f2492e53e3522439d62cf4f8e2c
Kuba429 <jakubzawadzki429@gmail.com>
2022-03-20 10:58:09 +0100

    minor changes making it easier to deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5001;
 const { Server } = require("socket.io");
 
 const server = app.listen(PORT, () => {
