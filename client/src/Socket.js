85bc8bdeed9a5fafb953706880678996bb64c8e4
Kuba429 <jakubzawadzki429@gmail.com>
2022-06-19 11:36:04 +0200

    moved server address to .env var

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SERVER = "ws://chat-ws-api.herokuapp.com/";
+const SERVER = import.meta.env.VITE_PROXY_WS_SERVER;
 // const SERVER = "ws://localhost:5001";
 
 const socket = io(SERVER, {
